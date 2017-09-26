1cbfa1c76f7d0532c13899072cacae3f17602571
sKanoodle <sKanoodle@users.noreply.github.com>
2017-09-26 17:05:34 +0200

    example recorder with classes for testing

---
+++ new_mfcrecorder.py
@@ -0,0 +1,26 @@
+import datetime
+import time
+import os
+import sys
+import classes.config
+import classes.models
+
+if __name__ == '__main__':
+    config = classes.config.Config(os.path.join(sys.path[0], 'config.conf'))
+    next_run = datetime.datetime.now()
+    while True:
+        if datetime.datetime.now() < next_run:
+            time.sleep(0.1)
+            continue
+        next_run += datetime.timedelta(seconds=config.settings.interval)
+        config.refresh()
+        print("another run {}".format(datetime.datetime.now()))
+
+        #live_models = {model for uid, model in classes.models.get_online_models().items()
+        #               if config.does_model_pass_filter(model.session)}
+        for uid, model in classes.models.get_online_models().items():
+            if not config.does_model_pass_filter(model):
+                continue
+            #classes.recording.start_recording(model.session, config.settings)
+            print("recording {}: {}".format(model.name, model.session['uid']))
+        print('finished run')
