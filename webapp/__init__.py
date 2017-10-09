c78c4132e2e2b71d2f5e2c4e6c998a430c870320
sKanoodle <sKanoodle@users.noreply.github.com>
2017-10-09 12:33:29 +0200

    added login logic to prevent unauthorized access

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_scss import Scss
 
 app = Flask(__name__)
+app.secret_key = 'set_this_to_something_secret' #TODO
 scss = Scss(app)
 scss.update_scss()
 
--- webapp/views.py
+++ webapp/views.py
@@ -5,6 +5,34 @@ def init_data(config):
     global CONFIG
     CONFIG = config
 
+def check_login():
+    print(flask.session.get('logged_in', None))
+    if flask.session.get('logged_in', None) is None:
+        return flask.redirect(flask.url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if flask.request.method == 'POST':
+        if flask.request.form['username'] != 'foo':
+            error = 'Invalid username'
+        elif flask.request.form['password'] != 'bar':
+            error = 'Invalid password'
+        else:
+            flask.session['logged_in'] = True
+            flask.flash('Successfully logged in!')
+            return flask.redirect(flask.url_for('start_page'))
+    return flask.render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    flask.session.pop('logged_in', None)
+    return flask.redirect(flask.url_for('start_page'))
+
+@app.route('/')
+def start_page():
+    return check_login() or flask.render_template('start_page.html')
+
 @app.route('/MFC/wanted')
 def wanted():
-    return flask.render_template('wanted.html', wanted=CONFIG.filter.wanted.dict)
+    return check_login() or flask.render_template('wanted.html', wanted=CONFIG.filter.wanted.dict)
