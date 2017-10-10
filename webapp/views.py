8257b4e3100ee598648f755125fd4d1eee2d5c9b
sKanoodle <sKanoodle@users.noreply.github.com>
2017-10-10 16:40:39 +0200

    added editing config in webapp

--- webapp/views.py
+++ webapp/views.py
@@ -35,3 +35,14 @@ def start_page():
 @app.route('/MFC/wanted')
 def wanted():
     return check_login() or flask.render_template('wanted.html', wanted=CONFIG.filter.wanted.dict)
+
+@app.route('/MFC/config', methods=['GET', 'POST'])
+def config():
+    check = check_login()
+    if check is not None:
+        return check
+
+    if flask.request.method == 'POST':
+        print(flask.request.form)
+
+    return flask.render_template('config.html', config=CONFIG)
