f69a728ee7360eea8ba780e402f80e5f49657ce4
beaston02 <beaston02@gmail.com>
2017-10-23 15:28:13 -0700

    Update convert.py
    fixed error cause when setting model data
    now adds models current display name to the models custom name field.

--- convert.py
+++ convert.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-import sys
-import os
+from mfcauto import Client
+import asyncio, sys, os
 import classes.config
 
-if len(sys.argv) < 2:
-    print('usage: convert.py <old_wanted> (relative paths only for now)')
-    exit()
-
 conf = classes.config.Config(os.path.join(sys.path[0], 'config.conf'))
-with open(os.path.join(sys.path[0], sys.argv[1])) as source:
-    for id in (int(line) for line in source.readlines()):
-        if id not in conf.filter.wanted.dict.keys():
-            conf.filter.wanted.set_data(id)
+async def run(loop):
+    client = Client(loop)
+    await client.connect(False)
+    with open(os.path.join(sys.path[0], sys.argv[1])) as source:
+        for id in (int(line) for line in source.readlines()):
+            if id not in conf.filter.wanted.dict.keys():
+                msg = await client.query_user(id)
+                if msg != None:
+                    conf.filter.wanted.add(uid=id, custom_name=msg['nm'])
+                    print("{} with uid {} added to list".format(msg['nm'], msg['uid']))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run(loop))
