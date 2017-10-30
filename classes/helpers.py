7dc9df252466f9795d9e5c982a1eff42a5f55716
sKanoodle <sKanoodle@users.noreply.github.com>
2017-10-30 09:20:20 +0100

    removed unused opencv code

--- classes/helpers.py
+++ classes/helpers.py
@@ -6,7 +6,6 @@ import hashlib
 import base64
 import enum
 import requests
-import cv2
 import numpy
 
 def try_eval(val):
@@ -54,33 +53,6 @@ def condition_text(condition, text='', upper=False):
         return '({})'.format(text)
     condition = texts[condition].upper() if upper else texts[condition]
     return condition
-
-def video_to_thumbnail(video_path):
-    '''returns content type and content as BytesIO or None'''
-    try:
-        cap = cv2.VideoCapture(video_path)
-    except:
-        logging.exception('cannot open video file')
-        return
-
-    if not cap.isOpened:
-        return
-
-    length = int(cap.get(cv2.CAP_PROP_FORMAT)) - 20
-    if (length > 0):
-        cap.set(cv2.CAP_PROP_POS_FRAMES, length)
-
-    success, image = cap.read()
-    if not success:
-        return
-
-    while success:
-        result = image
-        success, image = cap.read()
-
-    success, data = cv2.imencode('.png', result)
-    img = numpy.array(data).tostring()
-    return ('image/png', io.BytesIO(img))
 
 def get_avatar(uid):
     '''returns content type and content as BytesIO or None'''
